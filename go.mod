8c32e0429d17b7731aaa2d994385c7e1148fc5ca
mhrlife <pp2007ws@yahoo.com>
2024-02-02 14:43:59 +0330

    Initialize development environment

---
+++ go.mod
@@ -0,0 +1,15 @@
+module mazekav
+
+go 1.21
+
+require (
+	github.com/joho/godotenv v1.5.1
+	gorm.io/driver/mysql v1.5.2
+	gorm.io/gorm v1.25.6
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
+)
+
+func main() {
+	godotenv.Load()
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_DBNAME"),
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	fmt.Println(db, err)
+}
